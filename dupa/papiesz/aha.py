ebb74552405822fa0518cb78de33f05017490b2c
JuliaWitasiak <julia.witasiak@gmail.com>
2023-01-24 22:52:54 +0100

    initial commit

---
+++ dupa/papiesz/aha.py
@@ -0,0 +1,6 @@
+dupa = 2137
+print(dupa)
+haslo = ('dupadupa123')
+print(haslo)
+print(type(haslo))
+print(type(dupa))
---
+++ dupa/papiesz/syski.py
@@ -0,0 +1,15 @@
+# dupa = "69"
+# cycki = "52"
+# my_body = (dupa) + (cycki)
+# print(my_body)
+# print(type(cycki))
+ass = [69] * 10
+print(ass)
+yourmom = ["shit", "poop", "hellokitty"]
+print(yourmom)
+rupert = list(range(22))
+print (rupert)
+yourmom.append (1)
+# type() 
+print(yourmom)
+print(type(yourmom))
